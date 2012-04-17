2573ce2fd02af978a65e9e12a6c555e198d62617
Bram van Leur <bram@1vl.nl>
2012-04-17 17:46:07 +0200

    Move views directory and use normalize.css

--- app/app.php
+++ app/app.php
@@ -10,12 +10,12 @@ $app = new Silex\Application();
 
 /* Register the Twig Template Engine and use a global layout template */
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views',
+    'twig.path' => __DIR__.'/views',
     'twig.class_path' => __DIR__.'/../vendor/twig/lib',
 ));
 
 $app->before(function () use ($app) {
-    $app['twig']->addGlobal('layout', $app['twig']->loadTemplate('layout.twig'));
+    $app['twig']->addGlobal('layout', $app['twig']->loadTemplate('layout.html.twig'));
 });
 
 /* Load the controller definitions */
--- app/controllers.php
+++ app/controllers.php
@@ -1,5 +1,5 @@
 <?php
 
 $app->match('/', function () use ($app) {
-    return $app['twig']->render('index.twig');
+    return $app['twig']->render('index.html.twig');
 });
