6c6b006bbeb487862f5e1742a898894b36c79c02
Bram van Leur <bram@1vl.nl>
2012-06-07 10:44:42 +0200

    Improve config defaults

--- app/config.defaults.php
+++ app/config.defaults.php
@@ -3,8 +3,8 @@
 $app['content_path'] = realpath(__DIR__ . '/../content/');
 $app['web_path'] = realpath(__DIR__ . '/../web/');
 
-$app['maximum_width'] = 500;
+$app['maximum_width'] = 700;
 $app['maximum_height'] = 600;
 
-$app['imagine.driver'] = 'Gd'; // or Imagick or Gmagick
+$app['imagine.driver'] = 'Imagick'; // or Gmagick. Imagine also supports "Gd", but that comes with some limitations (extreme memory usage, no TIFF support)
 $app['updater.processing_step_seconds'] = 10;
