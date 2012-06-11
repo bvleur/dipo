70b1338afc0ea9460ba4561018b9839e0602f3f7
Bram van Leur <bram@1vl.nl>
2012-06-11 11:04:55 +0200

    Use configurable title.

--- app/config.defaults.php
+++ app/config.defaults.php
@@ -8,3 +8,5 @@ $app['maximum_height'] = 600;
 
 $app['imagine.driver'] = 'Imagick'; // or Gmagick. Imagine also supports "Gd", but that comes with some limitations (extreme memory usage, no TIFF support)
 $app['updater.processing_step_seconds'] = 10;
+
+$app['title'] = 'My Digital Portfolio';
