a4172ab4d4d70d0e70de97681b18927c9d7d59f8
Bram van Leur <bram@1vl.nl>
2012-06-19 00:49:40 +0200

    Refactored portfolio-browser.js and allow switching containers.
    Not yet a clear Model and View separation.

--- web/js/portfolio-browser.js
+++ web/js/portfolio-browser.js
@@ -1,14 +1,16 @@
 (function($) {
   "use strict";
 
-  $.fn.portfolioBrowser = function(initialId) {
-    var dotNav = this.find('.dot-nav li');
-    var container = this.find('.elementcontainer');
+  $.fn.portfolioBrowser = function(initialContainerCode, initialId) {
+    var dotNav = this.find('.dot-nav ul');
+    var portfolioNav = $('#menu, #tagcloud');
+    var viewContainer = this.find('.elementcontainer');
     var description = this.find('figcaption');
     var tagcontainer = this.find('.elementtags');
     var taglist = tagcontainer.find('ul');
-    var current = container.find('.element');
-    var currentIdx;
+    var current = viewContainer.find('.element');
+
+    var currentElement;
 
     /* Multiple elements in the container need to overlap during the transition.
      * Therefore elements need to be absolutely positioned in a relative positioned container.
@@ -26,76 +28,133 @@
       });
     }
 
-    container.css({'position': 'relative'});
+    viewContainer.css({'position': 'relative'});
     absoluteCenter(current);
 
-    /* List of elements */
-    var default_description;
-    var elements;
+    function generateURL(containerCode, id) {
+      return '/portfolio/' + encodeURIComponent(containerCode) + '/' +  encodeURIComponent(id);
+    }
 
-    function findElementIndex(id) {
-      for (var idx = 0; idx < elements.length; idx++)
-          if (elements[idx].id == id)
-            return idx;
+    function Container(code, data) {
+      this.code = code;
+      this.description = data.description;
+      this.elements = [];
+      for (var i = 0; i < data.elements.length; i++) {
+        this.elements.push(new Element(this, i, data.elements[i]));
+        if (i > 0) {
+          this.elements[i - 1].next = this.elements[i];
+          this.elements[i].previous = this.elements[i - 1];
+        }
+      }
+    }
 
-      // TODO not found: return a fake error element?
-      alert('not-found');
+    Container.prototype.getElement = function (id) {
+      for (var i = 0; i < this.elements.length; i++)
+          if (this.elements[i].id == id)
+            return this.elements[i];
+    }
+
+    function Element(container, idx, data) {
+      this.container = container;
+      this.idx = idx;
+      this.id = data.id;
+      this.description = data.description;
+      this.tags = data.tags;
+      this.html = data.html;
     }
 
     /* Get the staged (pre-loaded) element contents. If it isn't, stage it now
      * by adding the HTML to the container */
-    function getStagedElement(element) {
-      if (typeof element.staged === "undefined") {
-        var stagedHtml = $(element.html);
-        stagedHtml.css({'display': 'none'});
-        container.append(stagedHtml);
-        absoluteCenter(stagedHtml);
-        element.staged = stagedHtml;
-      }
+    Element.prototype.getStaged = function () {
+      if (this.staged)
+        return this.staged;
 
-      return element.staged;
+      this.staged = $(this.html)
+        .css({'display': 'none'})
+        .appendTo(viewContainer);
+
+      absoluteCenter(this.staged);
+
+      return this.staged;
     }
 
+    function ContainerList() {
+      this.containers = [];
+      return this;
+    }
+
+    ContainerList.prototype.withContainer = function (code, action) {
+      if ((code in this.containers)) {
+        action(this.containers[code]);
+        return;
+      }
+
+      var list = this;
+      $.getJSON(generateURL(code, 'browser-data'), function(data) {
+        list.containers[code] = new Container(code, data);
+        action(list.containers[code]);
+      });
+    }
+
+    var containers = new ContainerList();
 
     /* Transition the current element to element with id "id" and update meta-data and navigation */
-    function showElement(id, noPushState) {
-      /* Note: id isn't necessarily an element code. Element codes are only
-       * guaranteed to be unique in a group. It's a string uniquely identifying an element in a browsing set.
-       */
-      var idx = findElementIndex(id);
-      if (idx == currentIdx)
-        return; /* Nothing to do, this element is already shown. Don't animate */
+    Element.prototype.show = function (noPushState) {
+      var staged = this.getStaged();
 
-      var element = elements[idx];
-
-      var staged = getStagedElement(element);
+      /* Do not switch if the element to be shown is the same as the current */
+      if (current.is(staged))
+        return;
 
       /* Cross-fade the new element in (and hide the old afterwards) */
       staged.css({'display': 'block', 'opacity': 0});
       staged.animate({'opacity': 1});
+
       current.animate({'opacity': 0}, function () {
-        current.css({'display': 'none'});
-
-        /* The staged element should now be considered current as the old one is hidden */
-        current = staged;
-        currentIdx = idx;
-
+        $(this).css({'display': 'none'});
         preloadNextPrev();
       });
 
-      /* Update description */
-      description.html(element.description || default_description);
+      /* Update description */  
+      description.html(this.description || this.container.description);
 
       /* Update tags */
-      updateTags(element.tags)
+      updateTags(this.tags)
 
       /* Update navigation */
-      dotNav.eq(currentIdx).removeClass('current');
-      dotNav.eq(idx).addClass('current');
+      updateDotNav(this);
 
       /* Change the URL (ignore older browsers) */
-      if (Modernizr.history && noPushState)
-        window.history.pushState({id: id}, null, encodeURIComponent(id));
+      if (Modernizr.history && !noPushState) {
+        window.history.pushState({containerCode: this.container.code, id: this.id}, null,  generateURL(this.container.code, this.id));
+      }
+
+      /* This element should now be considered current */
+      current = staged;
+      currentElement = this;
+    }
+
+    function updateDotNav(element) {
+      if (element.container === currentElement.container) {
+        /* dotNav is already up to date, except that the old "current" is still marked */
+        dotNav.find('.current').removeClass('current');
+      } else {
+        /* recreate the dotNav for the current container */
+        var elements = element.container.elements;
+        dotNav.html('');
+        for (var i = 0; i < elements.length; i++) {
+          var dot = $('<li><a href="' + generateURL(elements[i].container.code, elements[i].id) + '">o</a></li>');
+          dot.data('element', elements[i]);
+          dot.click(function () {
+            $(this).data('element').show();
+            return false;
+          });
+          dotNav.append(dot);
+        }
+      }
+
+      /* Mark current */
+      dotNav.find('li').eq(element.idx).addClass('current');
     }
 
     function updateTags(elementTags) {
@@ -121,7 +180,7 @@
       $.each(toRemove, function (index, tag) { $(tag).remove(); });
       $.each(elementTags, function (index, elementTag) {
         if ($.inArray(index, notNew) === -1) {
-          var newTag = '<li><a href="/portfolio/' + elementTag.code + '/' + elementTag.firstElementId + '">' + elementTag.name + '</a></li>';
+          var newTag = '<li><a href="' + generateURL(elementTag.code, elementTag.firstElementId) + '">' + elementTag.name + '</a></li>';
           if (index == 0) {
             taglist.prepend(newTag);
           } else {
@@ -150,38 +209,55 @@
 
     /* Preload the previous and next elements by already requesting the element on stage */
     function preloadNextPrev() {
-      if ((currentIdx + 1) < elements.length) {
-        getStagedElement(elements[currentIdx + 1]);
+      if (currentElement.next) {
+        currentElement.next.getStaged();
       }
-      if ((currentIdx - 1) >= 0) {
-        getStagedElement(elements[currentIdx - 1]);
+      if (currentElement.previous) {
+        currentElement.previous.getStaged();
       }
+    }
+
+    function showByCodeAndId(containerCode, elementId, noPushState) {
+      containers.withContainer(containerCode, function(container) {
+        container.getElement(elementId).show(noPushState);
+      });
     }
 
     /* Make existing navigation elements in the page use this portfolio browser for switching */
     function attachToNavigation() {
       dotNav.find('a').click(function (e) {
-        showElement(decodeURIComponent($(this).attr('href')), true);
+        var id = decodeURIComponent($(this).attr('href'));
+        currentElement.container.getElement(id).show();
+        return false;
+      });
+
+      portfolioNav.find('a').click(function (e) {
+        portfolioNav.find('#current-page').removeAttr('id');
+        $(this).parent().attr('id', 'current-page');
+
+        var uri = decodeURIComponent($(this).attr('href'));
+        var parts = uri.match(/\/portfolio\/(.*)\/(.*)/);
+        showByCodeAndId(parts[1], parts[2]);
         return false;
       });
 
       if (Modernizr.history) {
         window.onpopstate = function (event) {
-          showElement(event.state.id);
+          showByCodeAndId(event.state.containerCode, event.state.id, true);
         }
-        window.history.replaceState({id: initialId}, null);
       }
-    }
+    };
 
     /* Retrive the data for the current browsing set */
-    $.getJSON('browser-data', function(data) {
-      default_description = data['description']
-      elements = data['elements'];
-      attachToNavigation();
+    containers.withContainer(initialContainerCode, function (container) {
+      currentElement = container.getElement(initialId);
+      currentElement.staged = current;
+      window.history.replaceState({containerCode: initialContainerCode, id: initialId}, null);
 
-      currentIdx = findElementIndex(initialId);
+      attachToNavigation();
       preloadNextPrev();
     });
-  };
+
+  }; // fn.portfolioBrowser
 
 })(jQuery);
